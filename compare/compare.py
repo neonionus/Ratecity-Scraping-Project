6a0ee55a272f08e148b58bc80d13ae7d46f24d2c
neonionus <ryancao11@gmail.com>
2018-06-08 17:05:18 +1000

    compare starting to work

--- compare/compare.py
+++ compare/compare.py
@@ -1,6 +1,4 @@
 import csv
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 import sys
 import re
 
@@ -10,7 +8,35 @@ def sanitise(row):
 		row[x] = str(row[x]).replace('$','')
 		row[x] = str(row[x]).replace(',','')
 		row[x] = str(row[x]).replace('.00','')
+		row[x] = str(row[x]).replace('n/a','0')
+		row[x] = str(row[x]).replace('N/A','0')
 		row[x] = str(row[x]).strip()
+	return row
+
+def sanitiseMZ(row, beta):
+	#sanitise take id and attach fixed term year bit
+	if row[beta] == str(0):
+		row[beta] = row['Product ID']
+	else:
+		row[beta] = re.sub(r'[A-Za-z\s]', '', row[beta])
+	if 'yes' in row['Extra repayments']:
+		row['Extra Repayments'] = "TRUE"
+	else:
+		row['Extra Repayments'] = "FALSE"
+
+	if 'yes' in row['Offset account']:
+		row['Offset account'] = "TRUE"
+	else:
+		row['Offset account'] = "FALSE"
+	
+	if 'yes' in row['Redraw facility']:
+		row['Redraw facility'] = "TRUE"
+	else:
+		row['Redraw facility'] = "FALSE"
+	
+	return row
+
+def sanitiseIF(row):
 	if 'Owner' in row['Availability']:
 		row['hasowneroccupied'] = 'True'
 	else:
@@ -60,13 +86,17 @@ def nameBuilder(row):
 
 ratecityList = {}
 infochoiceList = {}
+mozoList = {}
 companies = {}
 titles = {}
+codeMatch  = {}
 
 #builds dict of dicts (required as each row needs a unique identifier)
 with open('ratecity.csv', encoding='utf-8-sig') as ratecity:
 	reader = csv.DictReader(ratecity)
 	for row in reader:
+		row['rate'] = float(row['rate'])
+		#print(type(row['extrarepaymentsallowed']))
 		ratecityList.update({row['variationuuid']:row})
 
 with open('infochoice.csv', encoding='utf-8-sig') as infochoice:
@@ -75,74 +105,55 @@ with open('infochoice.csv', encoding='utf-8-sig') as infochoice:
 		#sanitisation
 		row['Name'] = nameBuilder(row)
 		row = sanitise(row)
+		row = sanitiseIF(row)
+		print(float(row['Rate']))
 		infochoiceList[row['CodeID']] = row
+
+with open('mozo.csv', encoding='utf-8-sig') as mozo:
+	reader = csv.DictReader(mozo)
+	for row in reader:
+		#sanitisation
+		row = sanitise(row)
+		row = sanitiseMZ(row, "Variation ID")
+		print(float(row['Interest Rate']))
+		mozoList[row['Variation ID']] = row
 
 with open('companies.csv', encoding='utf-8-sig') as company:
 	reader = csv.DictReader(company)
 	for row in reader:
-		companies.update({row['infochoice']:row['ratecity']})
+		companies[row['ratecity']] = row
 
 with open('titles.csv', encoding='utf-8-sig') as toot:
 	reader = csv.DictReader(toot)
 	for row in reader:
-		titles.update({row['RC']:row['IF']})
+		titles[row['RC']] = row
 
-#if matched csv is prepped
-if len(sys.argv) > 1:
-	if sys.argv[1] == "matched.csv":
-		matchedList = {}
-		with open('matched.csv', encoding='utf-8-sig') as matched:
-			reader = csv.DictReader(matched)
-			for row in reader:
-				matchedList.update({row['rc_code']:row['if_code']})
+with open('matched.csv', encoding='utf-8-sig') as tootie:
+	reader = csv.DictReader(tootie)
+	for row in reader:
+		row["mz_code"] = re.sub(r'[A-Za-z\s]', '', row["mz_code"])
+		codeMatch[row['rc_code']] = row
 
-		with open('fullmatch.csv', 'w+', newline='', encoding='utf-8-sig') as matcher:
-			csvwriter = csv.writer(matcher, delimiter=',')
-			headings = list(titles.keys())
-			headings.append("IF Code")
-			headings.append("IF Product")
-			print(headings)
-			csvwriter.writerow(headings)
-			for key, varRC in ratecityList.items():	
-				if key in matchedList.keys() and matchedList[key] != '0':
-					line = []
-					for titleRC, titleIF in titles.items():
-						if titleRC == 'companyname' or titleRC == 'variationname' or titleRC == 'variationuuid':
-							line.append(varRC[titleRC])
-						else:
-							if str(ratecityList[key][titleRC]).lower() == str(infochoiceList[matchedList[key]][titleIF]).lower():
-								line.append('MATCH')
-							else:
-								line.append(str(ratecityList[key][titleRC])+' vs '+str(infochoiceList[matchedList[key]][titleIF]))
-					line.append(matchedList[key])
-					line.append(infochoiceList[matchedList[key]]["Name"])
-					#print(line)
-					csvwriter.writerow(line)
-	else:
-		print("invalid argument")
-
-#first time string matching
-else:	
-	for varRC in ratecityList.values():
-		oldFuzz = 0
-		varRC['fuzzToken'] = 0
-		varRC['matchIF'] = 0
-		varRC['codeIF'] = 0
-		for varIF in infochoiceList.values():
-			if varIF['Company'] in companies.keys():
-				if (varRC['companyname'] == companies[varIF['Company']]
-				and varRC['homeloantype'] == varIF['Home Loan Type']
-				and ((varRC['hasowneroccupiedpurpose'] == varIF['hasowneroccupied']) or (varRC['hasinvestmentpurpose'] == varIF['hasinvestment']))):	
-					fuzzToken = fuzz.token_sort_ratio(varRC['variationname'], varIF['Name'])
-					if fuzzToken > 0 and fuzzToken > oldFuzz:
-						varRC['fuzzToken'] = fuzzToken
-						varRC['matchIF'] = varIF['Name']
-						varRC['codeIF'] = varIF['CodeID']
-						oldFuzz = fuzzToken
-						#print(varRC['matchIF'])
-
-	with open('matcher.csv', 'w+', newline='', encoding='utf-8-sig') as matcher:
+if len(sys.argv) != 2 or sys.argv[1] not in titles.keys():
+	print("Invalid arguments. Please give name of ratecityList field as argument.")
+else:
+	field = sys.argv[1]
+	name = field+".csv"	
+	with open(name, 'w+', newline='', encoding='utf-8-sig') as matcher:
 		csvwriter = csv.writer(matcher, delimiter=',')
-		csvwriter.writerow(["RC Code", "Ratecity", "IF Code", "Infochoice"])
-		for key, varRC in ratecityList.items():
-			csvwriter.writerow([key, varRC['Company Name + Variation Name'], varRC['codeIF'], varRC['matchIF']])
+		csvwriter.writerow(['rc_id','if_id','mz_id','company','variationname','rc_value','other_value'])
+		for keyRC, row in ratecityList.items():
+			if keyRC in codeMatch.keys():	
+				keyIF = codeMatch[keyRC]['if_code']
+				keyMZ = codeMatch[keyRC]['mz_code']
+				if keyIF == "0" or keyMZ == "0":
+					continue
+				if infochoiceList[keyIF][titles[field]['IF']] == mozoList[keyMZ][titles[field]['MZ']] != row[field]:
+					print(row[field])
+					print(row['companyname']+" "+row['variationname'])
+					line = [keyRC, keyIF, keyMZ, row['companyname'], row['variationname']]
+					line.append(row[field])
+					line.append(infochoiceList[keyIF][titles[field]['IF']])
+					csvwriter.writerow(line)
+	print("Success! compare.csv has been generated based on the field: "+field)
+	
